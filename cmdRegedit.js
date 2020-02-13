974c6ff05e652cde8f6c2ffa7f509191aeebbc4f
kopkan <375291152416@yandex.ru>
2020-02-14 01:01:17 +0300

    Update cmdRegedit.js

--- cmdRegedit.js
+++ cmdRegedit.js
@@ -22,116 +22,101 @@ function cmdRun(task, error){
 }
 
 
-
-function createTaskString(comand, keyName, valueName, value, valueType, isX64){
-    let valueNameStr = "";
-    if(valueName!=undefined && valueName!=null){
-        valueNameStr = "/v \""+ valueName +"\"";
-    }
-    let valueStr = "";
-    let valueTypeStr = "";
-    if(value!=undefined && value!=null){
-        valueStr = "/d \""+ value +"\"";
-    }
-    if(valueType!=undefined && valueType!=null){
-        valueTypeStr = "/d \""+ valueType +"\"";
-    }
-    let binStr = "";
-    if(isX64==true){ binStr = "/reg:64"; }
-    if(isX64==false){ binStr = "/reg:32"; }
-
-    let forseStr = " /f"
-    if(comand=="QUERY"){forseStr="";}
-
-    let taskString = "REG "+comand+" \""+keyName+"\" "+valueNameStr+" "+valueTypeStr+" "+valueStr+" "+binStr + " "+forseStr;
-    return taskString;
+function isSet(value){
+    return !( value==undefined || value==null );
 }
 
-function cmdRegeditCreate(keyName, valueName, value, valueType, isX64){
 
-    let task = createTaskString("ADD", keyName, valueName, value, valueType, isX64);
-    cmdRun(task);
-}
+class cmdRegedit{
+    constructor(parentKey, isX64){
+        this.parentKey = parentKey;
+        this.isX64 = isX64;
+    }
 
-function cmdRegeditRemove(keyName, valueName, isX64){
-    let task = createTaskString("DELETE", keyName, valueName, null, null, isX64);
-    cmdRun(task);
-}
+    _createTaskString(comand, keyName, valueName, value, valueType, isX64){
 
-function cmdRegeditQuery(keyName, valueName, isX64){
-
-    let task = createTaskString("QUERY", keyName, valueName, null, null, isX64);
-    let out = cmdRun(task);
-    if(out == false){return undefined;}
-
-
-    const dataAr = out.split('\r\n').filter(v => v).splice(1);
-    let valAr=[];
-    for (let i = 0; i < dataAr.length; i++) {
-        let str = dataAr[i];
-        let x1 = str.nthIndexOf("    ", 1);
-        let x2 = str.nthIndexOf("    ", 2);
-        let x3 = str.nthIndexOf("    ", 3);
-
-        let obj = new Object;
-        if(x1!=0){
-            obj.name = str;
-            obj.type = "KEY";
-            obj.value = str.slice(str.lastIndexOf("\\")+1);
+        if(!isSet(isX64)){
+            isX64 = this.isX64;
         }
-        else {
-            obj.name = str.slice(x1 + 4, x2);
-            obj.type = str.slice(x2 + 4, x3);
-            obj.value = str.slice(x3 + 4);
+        if(isSet(this.parentKey) && this.parentKey !=""){
+            if(keyName!=""){
+                keyName = this.parentKey + "\\" + keyName;
+            }
+            else{
+                keyName = this.parentKey;
+            }
         }
-        valAr.push(obj);
+
+
+        let taskString = "REG "+comand+" \""+keyName+"\"";
+        if(isSet(valueName)){
+            taskString += " /v \""+ valueName +"\"";
+        }
+        if(isSet(value)){
+            taskString += " /d \""+ value +"\"";
+        }
+        if(isSet(valueType)){
+            taskString += " /t \""+ valueType +"\"";
+        }
+        if(isX64==true){ taskString += " /reg:64"; }
+        if(isX64==false){ taskString += " /reg:32"; }
+        if(comand!="QUERY"){taskString +=" /f";}
+        return taskString;
     }
-    return valAr;
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function closeProcessInFolder(folder){
-
+    add(keyName, valueName, value, valueType, isX64){
+        let task = this._createTaskString("ADD", keyName, valueName, value, valueType, isX64);
+        return (cmdRun(task)!==false);
+    }
+    delete(keyName, valueName, isX64){
+        let task = this._createTaskString("DELETE", keyName, valueName, null, null, isX64);
+        return (cmdRun(task)!==false);
+    }
+    query(keyName, valueName, isX64){
+        let task = this._createTaskString("QUERY", keyName, valueName, null, null, isX64);
+        let out = cmdRun(task);
+        if(out === false){return undefined;}
     
-    folder = folder.replaceAll("\\\\\\\\", "\\");
-    folder = folder.replaceAll("\\\\\\", "\\");
-    folder = folder.replaceAll("\\\\", "\\");
-    folder = folder.replaceAll("/", "\\");
-    folder = folder.replaceAll("//", "\\");
-    folder = folder.replaceAll("\\", "\\\\");
-    /*
-    let newStr = "";
-    for(let i=0; i<str.length; i++){
-
+    
+        const dataAr = out.split('\r\n').filter(v => v).splice(1);
+        let valAr=[];
+        for (let i = 0; i < dataAr.length; i++) {
+            let str = dataAr[i];
+            let x1 = str.nthIndexOf("    ", 1);
+            let x2 = str.nthIndexOf("    ", 2);
+            let x3 = str.nthIndexOf("    ", 3);
+    
+            let obj = new Object;
+            if(x1!=0){
+                obj.name = str;
+                obj.type = "KEY";
+                obj.value = str.slice(str.lastIndexOf("\\")+1);
+            }
+            else {
+                obj.name = str.slice(x1 + 4, x2);
+                obj.type = str.slice(x2 + 4, x3);
+                obj.value = str.slice(x3 + 4);
+            }
+            valAr.push(obj);
+        }
+        return valAr;
     }
-    */
-   console.log(folder);
-
-    cmdRun("WMIC PROCESS WHERE \"ExecutablePath like '"+folder+"%%'\" DELETE");
-} 
-
-cmdRegeditCreate("HKLM\\SOFTWARE\\MyCo\\DDD\\1", "", "128", null, false);
-cmdRegeditRemove("HKLM\\SOFTWARE\\MyCo\\DDD\\1", null, false);
+    get(keyName, valueName, isX64){
+        let x = this.query(keyName, valueName, isX64);
+        if(x==undefined || !x.length){return;}
+        else{ return x[0].value; }
+    }
+}
 
 
-let x = cmdRegeditQuery("HKLM\\SOFTWARE\\MyCo\\DDD", null, false);
+
+/////////////////////
+let r = new cmdRegedit("", false);
+r.delete("HKLM\\SOFTWARE\\MyCo\\DDD\\1", null);
+r.add("HKLM\\SOFTWARE\\MyCo\\DDD\\1", null);
+
+let x = r.query("HKLM\\SOFTWARE\\MyCo\\DDD\\1", null);
 console.log(JSON.stringify(x));
-console.log(x[0].value);
 
-closeProcessInFolder("D:\\Dev\\Project\\js\\storm");
+let x2 = r.get("HKLM\\SOFTWARE\\MyCo\\DDD\\1", null);
+console.log(JSON.stringify(x2));
